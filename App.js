3f212f0c674445afefd81afa5f665126df0fa995
rubenmartinmorris <rubenmartinmorris@gmail.com>
2022-01-24 12:21:09 +0000

    login and create account button added

--- App.js
+++ App.js
@@ -1,34 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground, Image } from 'react-native';
-
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  Image,
+  Button,
+  TextInput,
+} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ImageBackground source={require('./img_assets/garden_generated.jpg')} resizeMode='cover' style={styles.img}>
-        <Image source={require('./img_assets/moaner.png')} style={styles.moaner}/>
-        <Image source={require('./img_assets/sick.png')} style={styles.sick}/>
-        <Image source={require('./img_assets/shouty.png')} style={styles.shouty}/>
-        <Image source={require('./img_assets/purple.png')} style={styles.purple}/>
-        
+      <ImageBackground
+        source={require('./img_assets/garden_generated.jpg')}
+        resizeMode='cover'
+        style={styles.img}
+      >
+        <View style={styles.buttons}>
+          <Button title={'login'} />
+          <Button title={'create account'} />
+        </View>
+        <View style={styles.imgBox}>
+          <Image
+            source={require('./img_assets/moaner.png')}
+            style={styles.moaner}
+          />
+          <Image
+            source={require('./img_assets/sick.png')}
+            style={styles.sick}
+          />
+          <Image
+            source={require('./img_assets/shouty.png')}
+            style={styles.shouty}
+          />
+          <Image
+            source={require('./img_assets/purple.png')}
+            style={styles.purple}
+          />
+        </View>
       </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  imgBox: {
+    flexDirection: 'row',
+    marginTop: 150,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 3,
+  },
   container: {
     flex: 1,
+    borderWidth: 3,
   },
   img: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: 'column',
     display: 'flex',
-    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    borderWidth: 3,
   },
   moaner: {
-    marginLeft: 10,
     marginBottom: 80,
     width: 100,
     height: 100,
@@ -50,5 +86,12 @@ const styles = StyleSheet.create({
     marginBottom: 50,
     width: 100,
     height: 100,
+  },
+  buttons: {
+    alignSelf: 'center',
+    borderWidth: 3,
+    justifyContent: 'space-evenly',
+    height: 200,
+    width: 300,
   },
 });

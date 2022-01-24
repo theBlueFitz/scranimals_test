7a45f07ad414002ae6ad4d7d07156d115c19673d
dappernerddesigns <verity.gregory@gmail.com>
2022-01-24 16:43:58 +0000

    faux logging almost working

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,64 +1,93 @@
-const { View, ImageBackground, Text } = require("react-native")
-import { StyleSheet } from "react-native";
+const {
+  View,
+  ImageBackground,
+  Text,
+  TextInput,
+  Button,
+  Pressable,
+} = require('react-native')
+import { StyleSheet } from 'react-native'
+import { useState } from 'react'
 
-export const LoginForm = ({navigation, route}) => {
-   return <View style={styles.container}>
-        <ImageBackground source={require('../img_assets/Autumn_Landscape.jpg')}
-        resizeMode='cover' style={styles.img}/>
+export const LoginForm = ({ navigation, route }) => {
+  const [user, setUser] = useState({ email: '', password: '' })
+  const handleChange = (e) => {
+    console.log(e.target.value)
+    console.dir(e.target.placeholder)
+    setUser((prevUser) => {
+      const copyUser = { ...prevUser }
+      copyUser[e.target.placeholder] = e.target.value
+      return copyUser
+    })
+    console.log(user)
+  }
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require('../img_assets/Autumn_Landscape.jpg')}
+        resizeMode="cover"
+        style={styles.img}
+      >
+        <View>
+          <TextInput
+            onChange={handleChange}
+            style={styles.textBoxes}
+            placeholder="email"
+            label={'email'}
+          />
+          <TextInput
+            onChange={handleChange}
+            style={styles.textBoxes}
+            placeholder="password"
+            label={'password'}
+          />
+        </View>
+        <Pressable title="Login" style={styles.buttons}>
+          <Text>Login!</Text>
+        </Pressable>
+      </ImageBackground>
     </View>
-
+  )
 }
 
 const styles = StyleSheet.create({
-    imgBox: {
-      flexDirection: 'row',
-      marginTop: 150,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    container: {
-      flex: 1,
-      
-    },
-    img: {
-      flex: 1,
-      flexDirection: 'column',
-      display: 'flex',
-      justifyContent: 'flex-end',
-      
-    },
-    moaner: {
-      marginBottom: 80,
-      width: 100,
-      height: 100,
-    },
-    shouty: {
-      marginLeft: -20,
-      marginBottom: 50,
-      width: 100,
-      height: 100,
-    },
-    purple: {
-      marginLeft: -20,
-      marginBottom: 80,
-      width: 100,
-      height: 100,
-    },
-    sick: {
-      marginLeft: -20,
-      marginBottom: 50,
-      width: 100,
-      height: 100,
-    },
-    buttons: {
-      alignSelf: 'center',
-      
-      justifyContent: 'space-evenly',
-      height: 200,
-      width: 300,
-    },
-    login: {
-      backgroundColor: '#000000',
-      color: '#000',
-    },
-  });
+  imgBox: {
+    flexDirection: 'row',
+    marginTop: 150,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container: {
+    flex: 1,
+  },
+  textBoxes: {
+    backgroundColor: '#ffffff',
+    border: 1,
+    width: 250,
+    height: 60,
+    margin: 10,
+    marginBottom: 20,
+    borderRadius: 10,
+  },
+  img: {
+    flex: 1,
+    flexDirection: 'column',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttons: {
+    alignSelf: 'center',
+    backgroundColor: '#0EAD69',
+    justifyContent: 'space-evenly',
+    height: 60,
+    width: 200,
+    borderRadius: 100 / 2,
+  },
+  login: {
+    backgroundColor: '#000000',
+    color: '#000',
+  },
+})

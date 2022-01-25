434df19889528cc80bcdc72ca6b135176585fdd6
karlaferry <karlacarreon.1994@gmail.com>
2022-01-25 16:55:52 +0000

    remove rogue backtick

--- components/PickPet.js
+++ components/PickPet.js
@@ -5,10 +5,10 @@ const {
   Text,
   TextInput,
   Button,
+  StyleSheet,
   Pressable,
   Image,
 } = require('react-native');
-import { styles } from '../Styles';
 import bg from '../img_assets/Autumn_Landscape.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import {
@@ -24,47 +24,72 @@ import petAvatar3 from '../img_assets/shouty.png';
 import petAvatar4 from '../img_assets/sick.png';
 import petAvatar2 from '../img_assets/purple.png';
 import { indexCarousel } from '../utils/utils';
-
 export const PickPet = ({ navigation, route }) => {
   const images = [petAvatar1, petAvatar2, petAvatar3, petAvatar4];
-
   const [petAvatarIndex, setPetAvatarIndex] = useState(0);
   return (
     <View style={styles.container}>
-      <ImageBackground source={bg} resizeMode='cover' style={styles.img}>
+      <ImageBackground
+        source={require('../img_assets/Autumn_Landscape.jpg')}
+        resizeMode='cover'
+        style={styles.img_bg}
+      >
         <View>
-          <Text>Pick Your Pet</Text>
+          <Text style={styles.header}>Pick Your Pet</Text>
         </View>
-        <View>
-          <Pressable onPress={() => {
+        <View style={styles.selectionBox}>
+          <Pressable
+            onPress={() => {
               setPetAvatarIndex((current) => {
-                console.log(current);
-                return indexCarousel(current, -1, 3)
+                return indexCarousel(current, -1, 3);
               });
-            }}>
-          <FontAwesomeIcon
-            icon={faChevronCircleLeft}
-          />
+            }}
+          >
+            <FontAwesomeIcon icon={faChevronCircleLeft} style={styles.arrows} />
           </Pressable>
-          <Image source={images[petAvatarIndex]} style={styles.sick} />
-          <Pressable onPress={() => {
+          <Image source={images[petAvatarIndex]} style={styles.petImage} />
+          <Pressable
+            onPress={() => {
               setPetAvatarIndex((current) => {
-                console.log(current);
                 return indexCarousel(current, 1, 3);
               });
-            }}>
-          <FontAwesomeIcon
-            icon={faChevronCircleRight}
-          /></Pressable>
+            }}
+          >
+            <FontAwesomeIcon
+              icon={faChevronCircleRight}
+              style={styles.arrows}
+            />
+          </Pressable>
         </View>
-        <Pressable
-          title='Pick'
-          style={styles.buttons}
-          onPress={() => navigation.navigate('Home')}
-        >
+        <Pressable title='Pick' onPress={() => navigation.navigate('Home')}>
           <Text>I Choose You</Text>
         </Pressable>
       </ImageBackground>
     </View>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  img_bg: {
+    flex: 1,
+    flexDirection: 'column',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  header: {
+    fontSize: 30,
+  },
+  selectionBox: {
+    flexDirection: 'row',
+  },
+  petImage: {
+    width: 150,
+    height: 150,
+  },
+  arrows: {
+    color: '#0EAD69',
+  },
+});

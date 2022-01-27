db83ad340ec2ef4e257a77299576144a61a4f9b4
theBlueFitz <seanyh92@gmail.com>
2022-01-27 15:15:19 +0000

    pulls shop data from firebase and iterates over

--- components/Shop.js
+++ components/Shop.js
@@ -1,12 +1,36 @@
 import { StyleSheet, View, Text } from 'react-native'
 import { getDatabase, ref, child, get } from 'firebase/database'
 import { app, database } from '../firebase'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import shopItems from '../utils/dbref'
 
 console.log(shopItems)
 export const Shop = () => {
   const [itemList, setItemList] = useState([])
+
+  useEffect(() => {
+    const dbRef = ref(database)
+    get(child(dbRef, `/Shop`))
+  .then((snapshot) => {
+    if (snapshot.exists()) {
+      setItemList(snapshot.val())
+    } else {
+      console.log('No data available')
+    }
+  })
+  .catch((error) => {
+    console.error
+  })
+  }, [])
+
+  console.log(itemList)
+  console.log(itemList.itemId0001)
+
+  for (let key in itemList) {
+    let value = itemList[key]
+    console.log(key, value)
+  }
+
 
   return (
     <View style={styles.container}>

341de9d1622082c955d825512275b6e1ee8b04c5
dappernerddesigns <verity.gregory@gmail.com>
2022-01-28 14:01:27 +0000

    nav menu showing and hiding correctly, routes needed

--- App.js
+++ App.js
@@ -1,35 +1,39 @@
-import React from 'react';
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { HomeScreen } from './components/HomeScreen.js';
-import { NavMenu } from './components/NavMenu.js';
-import { LoginForm } from './components/LoginForm';
-import { PickPet } from './components/PickPet';
-const Stack = createNativeStackNavigator();
-import app from './firebase';
+import { NavigationContainer } from '@react-navigation/native'
+import { navigationRef, navigate } from './routeNavigation'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { HomeScreen } from './components/HomeScreen.js'
+import { NavMenu } from './components/NavMenu.js'
+import { LoginForm } from './components/LoginForm'
+import { PickPet } from './components/PickPet'
+const Stack = createNativeStackNavigator()
+import app from './firebase'
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer ref={navigationRef}>
       <NavMenu />
       <Stack.Navigator>
         <Stack.Screen
-          name='Home'
+          name="Home"
           component={HomeScreen}
           options={{ headerShown: false }}
         />
+        {/* <Stack.Screen
+          name="NavMenu"
+          component={NavMenu}
+          options={{ headerShown: false }}
+        /> */}
         <Stack.Screen
-          name='LoginForm'
+          name="LoginForm"
           component={LoginForm}
           options={{ headerShown: false }}
         />
         <Stack.Screen
-          name='PickPet'
+          name="PickPet"
           component={PickPet}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
     </NavigationContainer>
-  );
+  )
 }
